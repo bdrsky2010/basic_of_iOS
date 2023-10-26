801fe203ff154d8b54caa4cc82c843b95a2f57cd
MJKim <qorhaqt@gmail.com>
2023-10-26 23:37:13 +0900

    Update: 아직 구현 미완료

---
+++ SwiftUI_Basic_of_Swift/DemoProject2/20231026/Storage/AppStorageNavigationSaveView.swift
@@ -0,0 +1,18 @@
+//
+//  AppStorageNavigationSaveView.swift
+//  DemoProject2
+//
+//  Created by Minjae Kim on 10/26/23.
+//
+
+import SwiftUI
+
+struct AppStorageNavigationSaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppStorageNavigationSaveView()
+}
