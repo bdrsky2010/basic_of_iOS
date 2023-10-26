6a4f89df2e3513c806a5147d205a99ad1cf07067
MJKim <qorhaqt@gmail.com>
2023-10-27 00:44:43 +0900

    Update: 코드리팩토링을 통해 ObservableObject class에 color data를 저장하는 프로퍼티 및 color data 디코딩 및 인코딩 메서드 이동 후 객체를 View에 environmentObject를 통해 뿌려주는 방식으로 변경

--- SwiftUI_Basic_of_Swift/DemoProject2/20231026/Storage/AppStorageColorPickerView.swift
+++ SwiftUI_Basic_of_Swift/DemoProject2/20231026/Storage/AppStorageColorPickerView.swift
@@ -7,50 +7,75 @@
 
 import SwiftUI
 
-struct AppStorageColorPickerView: View {
+class ColorData: ObservableObject {
+	// AppStorage를 통해 CGColor의 RGBA data를 문자열 형태로 Local Storage에 저장하는 프로퍼티
 	@AppStorage("myCGColor") var saveCGColor: String = ""
 	
-	@State var colorPick: CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
-	@State var loadColor: CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
-	
-	let colors: [Color] = [.black, .blue, .brown, .yellow]
-	let colornames: [String] = ["Black", "Blue", "Brown", "Yellow"]
-	
-	var body: some View {
-		VStack{
-			Spacer()
-			ColorPicker(selection: Binding(get: { colorPick }, set: { newValue in
-				self.colorPick = newValue
-			})) {
-				Text("색상변경")
-					.foregroundStyle(.white)
-					.font(.largeTitle)
-					.fontWeight(.bold)
-			}
-			.onChange(of: colorPick) { newValue in
-				print(newValue)
-				let cgColor: CGColor = newValue
-				guard let color = cgColor.components else { return }
-				self.saveCGColor = colorToString(color)
-			}
-			Spacer()
-		}
-		.onAppear {
-			self.loadColor = stringToColor(saveCGColor)
-			self.colorPick = loadColor
-		}
-		.background(Color(cgColor: colorPick))
-	}
+	// ColorPicker에서 pick한 color의 CGColor data 저장 및
+	// AppStorage에 저장한 CGColor를 다시 Load 하여 저장하는 프로퍼티
+	@Published var colorPick: CGColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0)
 	
 	func colorToString(_ color: [CGFloat]) -> String {
+		guard color != [] else { return "0 0 0 1" }
 		return "\(color[0]) \(color[1]) \(color[2]) \(color[3])"
 	}
 	
 	func stringToColor(_ color: String) -> CGColor {
-		guard !color.isEmpty else { return CGColor(red: 0, green: 0, blue: 0, alpha: 0) }
 		let rgba: [Float] = color.components(separatedBy: " ").map { Float($0) ?? 0 }
 		print(CGColor(red: CGFloat(rgba[0]), green: CGFloat(rgba[1]), blue: CGFloat(rgba[2]), alpha: CGFloat(1)))
 		return CGColor(red: CGFloat(rgba[0]), green: CGFloat(rgba[1]), blue: CGFloat(rgba[2]), alpha: CGFloat(1))
+	}
+}
+
+struct AppStorageColorChangeView: View {
+	@EnvironmentObject var colorData: ColorData
+	
+	var body: some View {
+		VStack {
+			Spacer()
+			Text("색 변경 확인 View")
+				.foregroundStyle(.white)
+				.font(.largeTitle)
+				.fontWeight(.bold)
+				.frame(maxWidth: .infinity)
+			Spacer()
+		}
+		.background(Color(cgColor: colorData.colorPick))
+	}
+}
+
+struct AppStorageColorPickerView: View {
+	// ObservableObject인 ColorData 객체를 가져오는 StateObject 프로퍼티
+	@StateObject var colorData: ColorData = ColorData()
+	
+	var body: some View {
+		VStack{
+			Spacer()
+			ColorPicker(selection: Binding(get: {
+				colorData.colorPick
+			}, set: { newValue in
+				print("picker", newValue)
+				colorData.colorPick = newValue
+			})) {
+				Text("색상변경")
+					.font(.largeTitle)
+					.fontWeight(.bold)
+			}
+			.padding()
+			.onChange(of: colorData.colorPick) { newValue in
+				let cgColor: [CGFloat] = newValue.components ?? []
+				colorData.saveCGColor = colorData.colorToString(cgColor)
+			}
+			NavigationLink(destination: AppStorageColorChangeView().environmentObject(colorData)) {
+				Text("색 변경 확인").frame(maxWidth: .infinity)
+			}
+			.buttonStyle(.borderedProminent)
+			.padding()
+			Spacer()
+		}
+		.onAppear {
+			colorData.colorPick = colorData.stringToColor(colorData.saveCGColor)
+		}
 	}
 }
 
