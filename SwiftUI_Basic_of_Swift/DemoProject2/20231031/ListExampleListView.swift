9830272c545337c8428945edc943fc2767ad2b05
MJKim <qorhaqt@gmail.com>
2023-11-01 20:39:51 +0900

    Modify: ListView에 대한 추가 예제 View NavigationLink로 연결

--- SwiftUI_Basic_of_Swift/DemoProject2/20231031/ListExampleListView.swift
+++ SwiftUI_Basic_of_Swift/DemoProject2/20231031/ListExampleListView.swift
@@ -25,6 +25,24 @@ struct ListExampleListView: View {
 			NavigationLink(destination: ListView5()) {
 				Text("NavigationStack List")
 			}
+			NavigationLink(destination: ListExamView1()) {
+				Text("List 예제 1번")
+			}
+			NavigationLink(destination: ListExamView2()) {
+				Text("List 예제 2번")
+			}
+			NavigationLink(destination: ListExamView3()) {
+				Text("List 예제 3번")
+			}
+			NavigationLink(destination: ListExamView4()) {
+				Text("List 예제 4번")
+			}
+			NavigationLink(destination: ListExamView5()) {
+				Text("List 예제 5번")
+			}
+			NavigationLink(destination: ListExamView6()) {
+				Text("List 예제 6번")
+			}
 		}
 	}
 }
