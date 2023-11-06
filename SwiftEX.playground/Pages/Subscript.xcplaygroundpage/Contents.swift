4af25f9fdae4fe220f79c7c5c79922d7ca12be1c
MJKim <qorhaqt@gmail.com>
2023-11-06 20:57:26 +0900

    Add: 스위프트 서브스크립트 학습

---
+++ SwiftEX.playground/Pages/Subscript.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+/*
+ 서브스크립트(Subscripts)
+ 콜렉션의 요소에 접근
+ 
+ 서브스크립트(Subscripts)는 클래스, 구조체, 열거형의 콜렉션/리스트/시퀀스의 멤버 요소에 접근할 수 있는 단축표현
+ 설정과 검색을 위한 별도의 메서드 없이 인덱스로 값을 설정하고 조회하기 위해 서브 스크립트를 사용
+ 
+ 예) someArray[index] 로 Array 인스턴스에 요소를 접근하고 someDictionary[key] 로 Dictionary 인스턴스에 요소를 접근
+ */
+
+struct MySubscripts {
+	var arr: [String] = []
+	
+	subscript(index: Int) -> String {
+		get { return arr[index] }
+		set { arr[index] = newValue }
+	}
+}
+
+var mysc = MySubscripts(arr: ["Park", "Kim", "Lee"])
+mysc[1]
+mysc[1] = "Minjae"
+mysc[1]
+
+extension String {
+	subscript (reCount: Int) -> String {
+		var str = ""
+		for _ in 0..<reCount {
+			str += self
+		}
+		return str
+	}
+}
+print( "Hello"[5] )
