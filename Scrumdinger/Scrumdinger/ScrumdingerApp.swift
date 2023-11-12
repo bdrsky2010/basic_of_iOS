86a10598b27ef072a9f8e47e2bcddc4d2afc5cd7
MJKim <qorhaqt@gmail.com>
2023-11-12 20:17:50 +0900

    Update: 챕터6 - 2 Persisting data 까지 완료

--- Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -1,19 +1,31 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Minjae Kim on 11/6/23.
-//
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
 
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-	@State private var scrums = DailyScrum.sampleData
-	
-	var body: some Scene {
-		WindowGroup {
-			ScrumsView(scrums: $scrums)
-		}
-	}
+	@StateObject private var store = ScrumStore()
+
+    var body: some Scene {
+        WindowGroup {
+			ScrumsView(scrums: $store.scrums) {
+				Task {
+					do {
+						try await store.save(scrums: store.scrums)
+					} catch {
+						fatalError(error.localizedDescription)
+					}
+				}
+			}
+				.task {
+					do {
+						try await store.load()
+					} catch {
+						fatalError(error.localizedDescription)
+					}
+				}
+        }
+    }
 }
