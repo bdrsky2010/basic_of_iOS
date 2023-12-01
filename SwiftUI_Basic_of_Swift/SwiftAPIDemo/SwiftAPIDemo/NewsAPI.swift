bd8fde3e266e9440ed0787115c920c2ba2819d7a
MJKim <qorhaqt@gmail.com>
2023-12-01 19:41:15 +0900

    [Modify] apikey값 가져오는 코드 수정

--- SwiftUI_Basic_of_Swift/SwiftAPIDemo/SwiftAPIDemo/NewsAPI.swift
+++ SwiftUI_Basic_of_Swift/SwiftAPIDemo/SwiftAPIDemo/NewsAPI.swift
@@ -24,25 +24,7 @@ class NewsAPI: ObservableObject {
 	
 	
 	private var apiKey: String? {
-		get {
-			let keyfilename = "ApiKeys"
-			let api_key = "API_KEY"
-			
-			// 생성한 .plist 파일 경로 불러오기
-			guard let filePath = Bundle.main.path(forResource: keyfilename, ofType: "plist") else {
-				fatalError("Couldn't find file '\(keyfilename).plist'")
-			}
-			
-			// .plist 파일 내용을 딕셔너리로 받아오기
-			let plist = NSDictionary(contentsOfFile: filePath)
-			
-			// 딕셔너리에서 키 찾기
-			guard let value = plist?.object(forKey: api_key) as? String else {
-				fatalError("Couldn't find key '\(api_key)'")
-			}
-			
-			return value
-		}
+		get { getValueOfPlistFile("ApiKeys", "API_KEY") }
 	}
 	
 	func feachData() {
