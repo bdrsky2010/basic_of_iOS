690d09af125d4c0d78c047b4cce71eee5756aef3
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-04-01 15:11:31 +0900

    [Modify] 함수의 지칭 및 표기법, 오버로딩 학습

--- SwiftEX.playground/Pages/Function-Deep.xcplaygroundpage/Contents.swift
+++ SwiftEX.playground/Pages/Function-Deep.xcplaygroundpage/Contents.swift
@@ -30,3 +30,67 @@ func introduce(name: String, age: Int, hobby: String...) -> Friend {
 }
 
 print(introduce(name: "마이콜", age: 35, hobby: "기타", "라면먹기", "작사", "작곡"))
+
+/**
+ # 함수를 지칭할 때, 함수 표기법
+ - 함수를 변수에 담는 경우
+ */
+
+func printSomething() {
+	print("어쩌구저쩌구")
+}
+
+func printMyName(name: String) {
+	print("제 이름은 \(name)입니다.")
+}
+
+func printFullName(lastName: String, firstName: String) {
+	print("성은 \(lastName)이고 이름은 \(firstName)입니다.")
+}
+
+func calcSquareArea(_ width: Int, _ height: Int) -> Int {
+	return width * height
+}
+
+// 1) 파라미터가 없는 경우 ()없이 표기
+printSomething
+var something = printSomething
+something()
+
+// 2) 파라미터가 있는 경우 아규먼트 레이블까지 표기
+printMyName(name:)
+var myName = printMyName(name:)
+myName("민재")
+
+// 3) 파라미터가 여러개인 경우, 콤마없이 (아규먼트 레이블:아규먼트 레이블) 처럼 표기
+printFullName(lastName:firstName:)
+var myFullName = printFullName(lastName:firstName:)
+myFullName("김", "민재")
+
+// 4) 아규먼트 레이블이 생략된 경우
+calcSquareArea(_:_:)
+var area = calcSquareArea(_:_:)
+area(20, 30)
+
+
+/**
+ # 함수의 타입 표기 방법
+ */
+var function1: () -> Void = printSomething
+var function2: (Int, Int) -> Int = calcSquareArea(_:_:)
+
+/**
+ # 함수의 오버로딩
+ - 동일한 이름을 가진 함수에 매개변수를 다르게 선언하여, 동일한 이름의 함수에 여러개의 함수를 사용할 수 있게 하는 것
+	- 함수 이름을 재사용하는 것
+ */
+
+func introduction(name: String) {
+	print("안녕하세요 저의 이름은 \(name)입니다.")
+}
+func introduction(name: String, age: Int) {
+	print("안녕하세요 저의 이름은 \(name)입니다.\n저의 나이는 \(age)살 이구요.")
+}
+func introduction(name: String, age: Int, hobby: String...) {
+	print("안녕하세요 저의 이름은 \(name)입니다.\n저의 나이는 \(age)살 이구요.\n저의 취미는 \(hobby)입니다.")
+}
